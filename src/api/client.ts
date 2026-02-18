a709d2a8258a87ec3bd32185b4976d674478b645
Varun <varun_gaikwad@jp.honda>
2026-02-18 09:18:54 +0900

    /api added

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "https://scenic-start-node-ten.vercel.app",
+  baseURL: "https://scenic-start-node-ten.vercel.app/api",
 });
 
 export default client;
